20842c41480cd6f5427df4391f0eef957b94e9be
Yogesh Gupta <Yogesh798714@gmial.com>
2025-02-07 11:13:13 +0530

    Updated API URL

--- email-sender/vite.config.js
+++ email-sender/vite.config.js
@@ -1,7 +1,11 @@
 export default defineConfig({
   server: {
     proxy: {
-      "/send-email": "nodemailer-production.up.railway.app", // Use your Railway backend URL
+      "/send-email": {
+        target: "https://nodemailer-production.up.railway.app", // Ensure it has HTTP/HTTPS
+        changeOrigin: true,
+        secure: false, // Set to false if your backend does not support HTTPS
+      },
     },
   },
 });
