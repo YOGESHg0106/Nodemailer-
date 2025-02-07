12a2d123e11fd80813feaf4e72daf1face83bf9d
Yogesh Gupta <Yogesh798714@gmial.com>
2025-02-07 11:03:46 +0530

    Updated API URL

--- email-sender/vite.config.js
+++ email-sender/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/send-email": "nodemailer-production.up.railway.app", // Use your Railway backend URL
+    },
+  },
+});
