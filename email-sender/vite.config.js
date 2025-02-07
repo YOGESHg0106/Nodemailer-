cd7dd608ce594c0acb975ca2e9e7d6139fc34e49
Yogesh Gupta <Yogesh798714@gmial.com>
2025-02-07 11:15:13 +0530

    Updated API URL

--- email-sender/vite.config.js
+++ email-sender/vite.config.js
@@ -1,10 +1,12 @@
+import { defineConfig } from "vite";
+
 export default defineConfig({
   server: {
     proxy: {
       "/send-email": {
-        target: "https://nodemailer-production.up.railway.app", // Ensure it has HTTP/HTTPS
+        target: "https://nodemailer-production.up.railway.app", // Ensure it's a valid URL
         changeOrigin: true,
-        secure: false, // Set to false if your backend does not support HTTPS
+        secure: false,
       },
     },
   },
